870e4aa919d95ea2c28f295522c02b779ed75042
Buddy <95647025+BuddyBuie@users.noreply.github.com>
2022-08-16 15:53:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var input = document.getElementById("inp1");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("btn1").click();
+  }
+});
+
+
+function pressed() { 
+  var text = parseInt(document.getElementById("inp1").value);
+    output.innerHTML =  (Math.round(text * 30) / 90).toFixed(0)
+
+}
+
+
+var input = document.getElementById("inp2");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("btn2").click();
+  }
+});
+
+
+function pressed1() { 
+  var text1 = parseInt(document.getElementById("inp2").value);
+    output1.innerHTML = (Math.round(text1 * 15) / 90).toFixed(0)
+
+}
